f373df73b4d7b1e7003970eff7f6be2240814cdb
Rifky <s22010240@ousl.lk>
2025-02-11 00:09:52 +0530

    styling-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 
-import './App.css'
+
+import ProjectsSidebar from './components/ProjectsSidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className=' bg-primary-500 p-0 m-0'>
+      <div>
+      <main className=' h-screen p-8 '>
+    <ProjectsSidebar/>
+   </main>
+      </div>
+
+      
+
+    </div>
+   
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#394578", // Lightest shade
+          200: "#1D284F",
+          300: "#121F3E",
+          400: "#1F2A50",
+          500: "#1C274E",
+          600: "#1E2950",
+          700: "#131F3F",
+          800: "#1B264D",
+          900: "#111F3C", // Darkest shade
+          950: "#162242"
+        },
+      },
+    },
   },
   plugins: [],
 }
